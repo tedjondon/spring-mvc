4cee48ed34388723c1f62378b86f700dddfe4109
Ted Jondon <ted.jondon@yandex.ru>
2021-11-12 14:17:11 +0300

    fixed cars.html

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
-
 import java.util.ArrayList;
 import java.util.List;
 
